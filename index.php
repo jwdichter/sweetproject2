46c9f7980c717566c1458f80ea3c7091e0cd80a2
jwdichter <jwdichter@mxschool.edu>
2015-01-23 23:43:22 -0500

    Added index.php and edited README.md

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    <h2> "sweetproject2" <h2>;
+    <p> "sweetproject2 is a revolutionary experience made by up and coming programmer Justin Dichter.  This code cannot be copied, reproduced, or manipulated in any way.  If you wish to admire the exquisite design of the website or the beautifully penned code, you may do so, but proceed with caution, as the little sweetproject code elves will be watching." <p>;
+    <img src="fathertimecat.jpg"/>
+    <ol> "The Top 5 Gatorade flavors"
+        <li> "Glacier Freeze" </li>
+        <li> "Cool Blue" </li>
+        <li> "Lemon-Lime" </li>
+        <li> "Fruit Punch" </li>
+        <li> "Orange" </li>
+    </ol>
+    <table>
+        <tr>
+          <th>"Month"</th>
+          <th>"Savings"</th>
+        </tr>
+        <tr>
+            <td>"January"</td>
+            <td>"$100"</td>
+        </tr>
+        <tr>
+          <td>"February"</td>
+          <td>"$80"</td>
+        </tr>
+        <tr>
+          <td>"March"</td>
+          <td>"$150"</td>
+        </tr>        
+        <tr>
+          <td>"April"</td>
+          <td>"$80"</td>
+        </tr>        
+        <tr>
+          <td>"May"</td>
+          <td>"$200"</td>
+        </tr>        
+        <tr>
+          <td>"June"</td>
+          <td>"$175"</td>
+        </tr>        
+        <tr>
+          <td>"July"</td>
+          <td>"$50"</td>
+        </tr>        
+    </table>
+    <a href = "https://www.google.com/"> Google! </a>
+?>
